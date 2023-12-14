ebd477dfbb66b8a99fbdb84395f6a939b88f19e5
Shehzad Raheem <shehzadraheem.sr38@gmail.com>
2023-12-14 22:12:06 +0500

    data_domain_setup

---
+++ lib/features/data/datasource/remote_data_source.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../../../core/constant/constant.dart';
+import '../../../core/error/exception.dart';
+import '../model/weather_model.dart';
+
+abstract class WeatherRemoteDataSource {
+  Future<WeatherModel> getCurrentWeather(String cityName);
+}
+
+
+class WeatherRemoteDataSourceImpl extends WeatherRemoteDataSource {
+  final http.Client client;
+  WeatherRemoteDataSourceImpl({required this.client});
+
+  @override
+  Future < WeatherModel > getCurrentWeather(String cityName) async {
+    final response =
+    await client.get(Uri.parse(Urls.currentWeatherByName(cityName)));
+
+    if (response.statusCode == 200) {
+      return WeatherModel.fromJson(json.decode(response.body));
+    } else {
+      throw ServerException();
+    }
+  }
+}
