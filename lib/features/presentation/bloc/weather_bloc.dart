6fbe39b8753fbdc340ed3e7d163d0aa6a2d72e59
Shehzad Raheem <shehzadraheem.sr38@gmail.com>
2023-12-19 21:11:05 +0500

    bloc_setup

---
+++ lib/features/presentation/bloc/weather_bloc.dart
@@ -0,0 +1,33 @@
+import 'package:bloc_weather/features/presentation/bloc/weather_event.dart';
+import 'package:bloc_weather/features/presentation/bloc/weather_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rxdart/rxdart.dart';
+import '../../domain/usecases/get_current_weather.dart';
+
+
+class WeatherBloc extends Bloc<WeatherEvent,WeatherState> {
+
+  final GetCurrentWeatherUseCase _getCurrentWeatherUseCase;
+  WeatherBloc(this._getCurrentWeatherUseCase) : super(WeatherEmpty()) {
+    on<OnCityChanged>(
+          (event, emit) async {
+
+        emit(WeatherLoading());
+        final result = await _getCurrentWeatherUseCase.execute(event.cityName);
+        result.fold(
+              (failure) {
+            emit(WeatherLoadFailue(failure.message));
+          },
+              (data) {
+            emit(WeatherLoaded(data));
+          },
+        );
+      },
+      transformer: debounce(const Duration(milliseconds: 500)),
+    );
+  }
+}
+
+EventTransformer<T> debounce<T>(Duration duration) {
+  return (events, mapper) => events.debounceTime(duration).flatMap(mapper);
+}
